050844aac4c9075f61d02d933bca771229170c62
hokekiyoo <imslotter1@gmail.com>
2017-08-18 23:13:51 +0900

    add button in popup

--- background.js
+++ background.js
@@ -12,3 +12,12 @@ chrome.runtime.onMessage.addListener(
     );
   }
 );
+
+// popupのボタンによって。
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.greeting == "hello")
+      sendResponse({
+        msg: "goodbye!"
+      });
+  });
