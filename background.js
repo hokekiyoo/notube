14dd6532da6cfce3e054ecdc7f51a29c0ba8afb2
hokekiyoo <imslotter1@gmail.com>
2017-08-21 01:57:08 +0900

    delete tabs in all chrome-window

--- background.js
+++ background.js
@@ -4,28 +4,33 @@ chrome.runtime.onMessage.addListener(
     var min = parseFloat(localStorage.getItem("time"))*1000*60;
     setTimeout(
       function () {
-        chrome.tabs.getAllInWindow(null, function(tabs){
-          for (var i = 0; i < tabs.length; i++){
-            if (tabs[i].url.match(/www.youtube.com/)){
-              if(request.value["youtube_checked"]){
-                chrome.tabs.remove(tabs[i].id);
+        chrome.windows.getAll(null, function(windows){
+          for (var i=0; i<windows.length; i++) {
+            var winId = windows[i].id;
+            chrome.tabs.getAllInWindow(winId, function(tabs){
+              for (var i = 0; i < tabs.length; i++){
+                if (tabs[i].url.match(/www.youtube.com/)){
+                  if(request.value["youtube_checked"]){
+                    chrome.tabs.remove(tabs[i].id);
+                  }
+                }
+                if (tabs[i].url.match(/twitter.com/)){
+                  if(request.value["twitter_checked"]){
+                    chrome.tabs.remove(tabs[i].id);
+                  }        
+                }
               }
-            }
-            if (tabs[i].url.match(/twitter.com/)){
-              if(request.value["twitter_checked"]){
-                chrome.tabs.remove(tabs[i].id);
-              }        
-            }
-          }
+          });
+        }
         if(localStorage.getItem("url")==null||localStorage.getItem("url")==""){
           window.open("http://www.procrasist.com", '_blank');
         }
         else{
           window.open(localStorage.getItem("url"), '_blank');
         }
-      localStorage.setItem("timer_on",false)
-      alert("ダメダヨ");
-      });
+        localStorage.setItem("timer_on","OFF")
+        alert("ダメダヨ");
+        });
     },min);
   });
 
