0d472f4b92278e584b679e953cdcdf53e31e2b75
hokekiyoo <imslotter1@gmail.com>
2017-08-21 01:20:31 +0900

    move setTimeout from popup.js to background.js

--- background.js
+++ background.js
@@ -1,25 +1,31 @@
 // popupのボタンによって。
 chrome.runtime.onMessage.addListener(
-  function (request, sender, sendResponse) {
-    alert("ダメダヨ");
-    chrome.tabs.getAllInWindow(null, function(tabs){
-      for (var i = 0; i < tabs.length; i++){
-        if (tabs[i].url.match(/www.youtube.com/)){
-          if(request.value["youtube_checked"]){
-            chrome.tabs.remove(tabs[i].id);
+  function(request, sender, sendResponse) {
+    var min = parseFloat(localStorage.getItem("time"))*1000*60;
+    setTimeout(
+      function () {
+        chrome.tabs.getAllInWindow(null, function(tabs){
+          for (var i = 0; i < tabs.length; i++){
+            if (tabs[i].url.match(/www.youtube.com/)){
+              if(request.value["youtube_checked"]){
+                chrome.tabs.remove(tabs[i].id);
+              }
+            }
+            if (tabs[i].url.match(/twitter.com/)){
+              if(request.value["twitter_checked"]){
+                chrome.tabs.remove(tabs[i].id);
+              }        
+            }
           }
+        if(localStorage.getItem("url")==null||localStorage.getItem("url")==""){
+          window.open("http://www.procrasist.com", '_blank');
         }
-        if (tabs[i].url.match(/twitter.com/)){
-          if(request.value["twitter_checked"]){
-            chrome.tabs.remove(tabs[i].id);
-          }        
+        else{
+          window.open(localStorage.getItem("url"), '_blank');
         }
-      }
-    if(localStorage.getItem("url")==null||localStorage.getItem("url")==""){
-      window.open("http://www.procrasist.com", '_blank');
-    }
-    else{
-      window.open(localStorage.getItem("url"), '_blank');
-    }
+      localStorage.setItem("timer_on",false)
+      alert("ダメダヨ");
+      });
+    },min);
   });
-});
+
--- popup.js
+++ popup.js
@@ -1,3 +1,21 @@
+// URLと時間の保存された設定
+document.getElementById("txt_url").value = localStorage.getItem("url") || "http://www.procrasist.com/";
+document.getElementById("txt_time").value = localStorage.getItem("time") || 1;
+
+// 時刻の取得
+var div = document.getElementById("textDiv");
+var now_t = new Date();
+diff_min = now_t.getTime() - localStorage.getItem("start_time");
+var remain = parseInt(localStorage.getItem("time") - diff_min/(1000*60));
+if(remain>=0){
+  div.textContent = "REMAINING : "+  remain+ " min";
+  // document.getElementById("btn_start").disabled = "true";
+}else{
+  div.textContent = "PUSH BUTTON";
+  document.getElementById("btn_start").disabled = "";
+}
+var text = div.textContent; 
+
 // イベントリスナーを table に追加
 var el = document.getElementById("btn_register");
 el.addEventListener("click", regist, false);
@@ -10,13 +28,14 @@ function regist() {
 }
 // スタート
 var start = document.getElementById('btn_start');
-// start.addEventListener("click", setTimeout(send, localStorage.getItem("time"), false);
 start.addEventListener("click", function(){
-  var min = parseFloat(localStorage.getItem("time"))*1000*60;
-  setTimeout(send,min);
-  }, false);
-function send(){
-  
+  var start_t = new Date();
+  localStorage.setItem("timer_on", true);
+  localStorage.setItem("start_time", start_t.getTime());
+  send();
+}, false);
+
+function send(){  
   chrome.runtime.sendMessage({
       type: "close_youtube",
       value: {"youtube_checked":kill.youtube.checked,
